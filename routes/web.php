2c9c9e8f8a11e6bc35e07e91e4b6a5b73f9653e9
ANNA MARONE <annamarone72@gmail.com>
2024-06-12 13:03:40 +0000

    integration du portail

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortailController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PortailController::class,'portail'] );
