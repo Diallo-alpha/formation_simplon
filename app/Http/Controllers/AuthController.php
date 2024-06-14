0f9d3f3a005a80456a12179dbdc4d2d175c95743
artefarte001 <cheikhsane656@gmail.com>
2024-06-14 16:56:05 +0000

    purification des codes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,17 +16,14 @@ class AuthController extends Controller
 
     public function postRegister(Request $request)
     {
-        // Validez et créez l'utilisateur
-        // $request->validate([
-        //     'nam' => 'required|string|max:255',
-        //     'email' => 'required|string|email|unique:users|max:255',
-        //     'password' => 'required|string|min:4|max:255',
-        // ]);
+        $request->validate([
+
+        ]);
 
         // Créer l'utilisateur
 
+       
         User::create($request->all() );
-
 
 
         return redirect()->route('auth.getLogin');
@@ -36,43 +33,50 @@ class AuthController extends Controller
             return view('login');
         }
 
-
     public function getLogin()
     {
         return view('Auth.login'); // Retournez la vue de connexion
     }
 
     public function postLogin(Request $request)
-    {
-        // Validez les informations d'identification
-        // $credentials = $request->validate([
-        //     'email' => 'required|string|email',
-        //     'password' => 'required|string',
-        // ]);
+{
+    // Validez les informations d'identification
+    $credentials = $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+    ]);
 
-        // $credentials = $request->validate([
-        //     'nom' => 'required|string|max:255',
-        //     'email' => 'required|string|email|unique:users|max:255',
-        //     'password' => 'required|string|min:4|max:255|regex:/[A-Z]+.*[a-z]+.*[0-9]+.*[\W_]+/',
-        // ]);
+    // Connectez l'utilisateur
+    if (Auth::attempt($credentials)) {
+        // Regénérer la session pour éviter les attaques de fixation de session
+        $request->session()->regenerate();
 
+        // Récupérer l'utilisateur connecté
+        $user = Auth::user();
 
-        // Connectez l'utilisateur
-        // if (Auth::attempt($credentials)) {
-        //     $request->session()->regenerate();
-        //     // Redirigez vers la page de tableau de bord ou une autre page
-        //     return redirect()->intended('offre');
-        // }
-
-        // Si l'authentification échoue, redirigez vers la page de connexion avec un message d'erreur
-        return back()->withErrors([
-            'email' => 'Les informations d\'identification fournies ne correspondent pas à nos enregistrements.',
-        ]);
+        // Vérifiez le rôle de l'utilisateur et redirigez en conséquence
+        switch ($user->role) {
+            case 'personnel':
+                return redirect('formationAdsbord');
+            case 'candidat':
+                return redirect('formulaire_postuler ');
+            default:
+                // Redirigez vers une page par défaut ou de tableau de bord si le rôle n'est pas défini
+                return redirect()->intended('offre');
+        }
     }
+
+    // Si l'authentification échoue, redirigez vers la page de connexion avec un message d'erreur
+    return back()->withErrors([
+        'email' => 'Les informations d\'identification fournies ne correspondent pas à nos enregistrements.',
+    ]);
+}
+
 
     public function logout()
     {
         Auth::logout(); // Déconnectez l'utilisateur
         return redirect()->route('auth.getLogin'); // Redirigez vers la page de connexion
-    }
+}
+
 }
